d580c3922f1d06d581f1e8be66254fc8c0d60bb0
Steve Morley <smorley@lanl.gov>
2015-11-06 02:09:00 -0700

    Plot1: added R script to generate plot (screen device)

---
+++ Plot1.R
@@ -0,0 +1,32 @@
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+#read data and
+#convert date field to date datatype
+require(data.table)
+getData <- function() {
+    powerdata <- data.table::fread('household_power_consumption.txt', na.strings='?')
+    #Plot 1 is a histogram of the power on 2 days: 2007-02-01 and 2007-02-02
+    #so select just these days and then convert to dates/times
+    d1 <- "1/2/2007"
+    d2 <- "2/2/2007"
+    #cut down to only required indices
+    subdata <- subset(powerdata, powerdata$Date==d1 | powerdata$Date==d2)
+    #add POSIXct times to data.table (not necessary for Plot1, but ideally I'd have one read function used
+    #by all the plots for this time period...)
+    subdata[, as.POSIXct(strptime(paste(subdata$Date, subdata$Time), '%d/%m/%Y %H:%M:%S'))]
+    #strptime is about 8x faster than as.Date
+    return(subdata)
+}
+
+##Now the data prep is done, make the plot (callable so the file can be sourced
+##and the function can be used separately...
+makePlot <- function(indata, ...) {
+    hist(indata, ...)
+}
+
+##Now run the thing...
+usedata <- getData()
+title <- "Global Active Power"
+xunits <- "(kilowatts)"
+xlabel <- paste(title, xunits)
+makePlot(usedata$Global_active_power, col='red', xlab=xlabel, main=title)
