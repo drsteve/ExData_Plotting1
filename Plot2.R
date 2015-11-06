c8010f79de4eee970c6bf26903d9447bb030279d
Steve Morley <smorley@lanl.gov>
2015-11-06 02:35:36 -0700

    Plot2: initial commit of script and figure, some tidying to do

---
+++ Plot2.R
@@ -0,0 +1,33 @@
+#Data downloaded from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+#read data and
+#convert date field to date datatype
+require(data.table)
+getData <- function() {
+    powerdata <- data.table::fread('household_power_consumption.txt', na.strings='?')
+    #Plot 2 is a time series of the power on 2 days: 2007-02-01 and 2007-02-02
+    #so select just these days and then convert to dates/times
+    d1 <- "1/2/2007"
+    d2 <- "2/2/2007"
+    #cut down to only required indices
+    subdata <- subset(powerdata, powerdata$Date==d1 | powerdata$Date==d2)
+    #add POSIXct times to data.table 
+    subdata[, "DateTime":=as.POSIXct(strptime(paste(subdata$Date, subdata$Time), '%d/%m/%Y %H:%M:%S'))]
+    #strptime is about 8x faster than as.Date
+    return(subdata)
+}
+
+##Now the data prep is done, make the plot (callable so the file can be sourced
+##and the function can be used separately...
+makePlot <- function(xquan, yquan, ...) {
+    plot(xquan, yquan, ...)
+}
+
+##Now run the thing...
+usedata <- getData()
+##Make labels
+ylabel <- "Global Active Power (kilowatts)"
+##Draw figure
+png('Plot2.png') #open new PNG output graphics device
+makePlot(usedata$DateTime, usedata$Global_active_power, type='l', xlab='',ylab=ylabel)
+dev.off() #close current output graphics device
